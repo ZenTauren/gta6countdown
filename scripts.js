df346be4cea828f1ed4e954cb45a93d8bedfe90b
ZythDr <45781696+ZythDr@users.noreply.github.com>
2024-03-20 23:51:53 +0100

    Variables removed

--- scripts.js
+++ scripts.js
@@ -1,20 +1,4 @@
 $(document).ready(function() {
-    var faviconURL = "favicon.ico";
-    var bannerLink = "images/banner.png";
-    var twitterLink = "https://twitter.com/RockstarGames?ref_src=twsrc%5Etfw";
-    var youtubeVideoLink = "https://www.youtube.com/embed/pUotKi9N7bc?autoplay=1&controls=0&fs=0&loop=1&modestbranding=1&color=white&iv_load_policy=3";
-    var chatroomURL = "https://kiwiirc.hybridirc.com/#GTAVIWaitingRoom";
-
-    $("link[rel='icon']").attr("href", faviconURL);
-    $("#bannerLink").attr("href", bannerLink).children("img").attr("src", bannerLink);
-    $("a.twitter-timeline").attr("href", twitterLink);
-    $("#youtubeVideo").attr("src", youtubeVideoLink);
-    $("#chatroomIframe").attr("src", chatroomURL);
-
-    var twitterWrapper = $('.twitter-wrapper');
-    var chatroomWrapper = $('.countdown-chatroom');
-    var popupDisplayed = false;
-    var popup = $('#popup');
   
     $('#toggleTweet').click(function() {
       twitterWrapper.css('left', twitterWrapper.css('left') === '0px' ? '-100%' : '0px');
