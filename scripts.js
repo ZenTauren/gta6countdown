d465d87cf5585420e3d282af1674901b57111b9d
ZythDr <45781696+ZythDr@users.noreply.github.com>
2024-03-21 00:13:02 +0100

    Major weight loss!
    scripts.js has made great progress on their weight loss journey and is now nearly skin and bones!

--- scripts.js
+++ scripts.js
@@ -1,20 +1,4 @@
 $(document).ready(function() {
-    var faviconURL = "favicon.ico";
-    var bannerLink = "images/banner.png";
-    var twitterLink = "https://twitter.com/RockstarGames?ref_src=twsrc%5Etfw";
-    var youtubeVideoLink = "https://www.youtube.com/embed/pUotKi9N7bc?autoplay=1&controls=0&fs=0&loop=1&modestbranding=1&color=white&iv_load_policy=3";
-    var chatroomURL = "https://kiwiirc.hybridirc.com/#GTAVIWaitingRoom";
-
-    $("link[rel='icon']").attr("href", faviconURL);
-    $("#bannerLink").attr("href", bannerLink).children("img").attr("src", bannerLink);
-    $("a.twitter-timeline").attr("href", twitterLink);
-    $("#youtubeVideo").attr("src", youtubeVideoLink);
-    $("#chatroomIframe").attr("src", chatroomURL);
-
-    var twitterWrapper = $('.twitter-wrapper');
-    var chatroomWrapper = $('.countdown-chatroom');
-    var popupDisplayed = false;
-    var popup = $('#popup');
   
     $('#toggleTweet').click(function() {
       twitterWrapper.css('left', twitterWrapper.css('left') === '0px' ? '-100%' : '0px');
@@ -22,26 +6,5 @@ $(document).ready(function() {
 
     $('#toggleChat').click(function() {
       chatroomWrapper.css('right', chatroomWrapper.css('right') === '0px' ? '-100%' : '0px');
-    });
-
-    if ($(window).height() <= 900 && !popupDisplayed) {
-      popup.show();
-      popupDisplayed = true;
-
-      $('#progress-bar').remove();
-      popup.append($('<div id="progress-bar"></div>'));
-
-      setTimeout(function() {
-        popup.hide();
-        popupDisplayed = false;
-      }, 5000);
-    } else if ($(window).height() > 900) {
-      popup.hide();
-      popupDisplayed = false;
-    }
-
-    $("#dismiss").click(function() {
-      $("#popup").hide();
-      popupDisplayed = false;
     });
 });
