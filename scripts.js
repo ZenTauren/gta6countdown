3cce77efbc550f565072dc9035ea464a44d80002
ZythDr <45781696+ZythDr@users.noreply.github.com>
2024-03-20 23:18:34 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     var bannerLink = "https://github.com/ZenTauren/gta6countdown/raw/main/images/GTAVI.png";
     var twitterLink = "https://twitter.com/RockstarGames?ref_src=twsrc%5Etfw";
     var youtubeVideoLink = "https://www.youtube.com/embed/pUotKi9N7bc?autoplay=1&controls=0&fs=0&loop=1&modestbranding=1&color=white&iv_load_policy=3";
+    var chatroomURL = "https://kiwiirc.hybridirc.com/#GTAVIWaitingRoom";
 
     $("link[rel='icon']").attr("href", faviconURL);
     $("#bannerLink").attr("href", bannerLink).children("img").attr("src", bannerLink);
     $("a.twitter-timeline").attr("href", twitterLink);
     $("#youtubeVideo").attr("src", youtubeVideoLink);
+    $("#chatroomIframe").attr("src", chatroomURL);
 
     var twitterWrapper = $('.twitter-wrapper');
     var chatroomWrapper = $('.countdown-chatroom');
