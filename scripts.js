89b16925cb23fcf28ef3a25c0036adb594c61b8e
ZythDr <45781696+ZythDr@users.noreply.github.com>
2024-03-22 00:03:02 +0100

    The Heimerdonger IQ Upgrade!
    This makes for a total of 3!

--- scripts.js
+++ scripts.js
@@ -1,70 +1,77 @@
-$(document).ready(function() {
-  var twitterWrapper = $(".twitter-wrapper");
-  var chatroomWrapper = $("#chatroomWrapper");
-  var isAnimating = false;  // this variable ensures animations don't overlap
+$(document).ready(function () {
+	var twitterWrapper = $(".twitter-wrapper");
+	var chatroomWrapper = $("#chatroomWrapper");
+	var isAnimating = false; // this variable ensures animations don't overlap
 
-  function toggleTwitter() {
-    if (!isAnimating) {
-      isAnimating = true;
-      
-      if (twitterWrapper.css('left') === "0px") {
-        twitterWrapper.animate({ left: "-100%" }, 200, function() {
-          isAnimating = false;
-        });
-      } else {
-        twitterWrapper.animate({ left: "0px" }, 200, function() {
-          isAnimating = false;
-        });
-      }
-    }
-  }
+	function toggleTwitter() {
+		if (!isAnimating) {
+			isAnimating = true;
 
-  function toggleChat() {
-    if(!isAnimating) {
-      isAnimating = true;
-      
-      if (chatroomWrapper.css('right') === "0px") {
-        chatroomWrapper.animate({ right: "-100%" }, 200, function() {
-          isAnimating = false;
-        });
-      } else {
-        chatroomWrapper.animate({ right: "0px" }, 200, function() {
-          isAnimating = false;
-        });
-      }
-    }
-  }
+			if (twitterWrapper.css("left") === "0px") {
+				twitterWrapper.animate({ left: "-100%" }, 300, function () {
+					isAnimating = false;
+				});
+			} else {
+				twitterWrapper.animate({ left: "0px" }, 300, function () {
+					isAnimating = false;
+				});
+			}
+		}
+	}
 
-  $('#toggleTweet').click(toggleTwitter);
-  $('#toggleChat').click(toggleChat);
+	function toggleChat() {
+		if (!isAnimating) {
+			isAnimating = true;
 
-  $(window).resize(function() {
-    var windowWidth = $(window).width();
+			if (chatroomWrapper.css("right") === "0px") {
+				chatroomWrapper.animate({ right: "-100%" }, 300, function () {
+					isAnimating = false;
+				});
+			} else {
+				chatroomWrapper.animate({ right: "0px" }, 300, function () {
+					isAnimating = false;
+				});
+			}
+		}
+	}
 
-    if (windowWidth < 1650) {
-      if (twitterWrapper.css('left') === "0px") {
-        toggleTwitter();
-      }
-      if (chatroomWrapper.css('right') === "0px") {
-        toggleChat();
-      }
-    } else {
-      if (twitterWrapper.css('left') !== "0px") {
-        toggleTwitter();
-      }
-      if (chatroomWrapper.css('right') !== "0px") {
-        toggleChat();
-      }
-    }
-  }).resize();
+	$("#toggleTweet").click(toggleTwitter);
+	$("#toggleChat").click(toggleChat);
+
+	$(window)
+		.resize(function () {
+			var windowWidth = $(window).width();
+
+			if (windowWidth < 1500) {
+				if (twitterWrapper.css("left") === "0px") {
+					toggleTwitter();
+				}
+				if (chatroomWrapper.css("right") === "0px") {
+					toggleChat();
+				}
+			} else {
+				if (twitterWrapper.css("left") !== "0px") {
+					toggleTwitter();
+				}
+				if (chatroomWrapper.css("right") !== "0px") {
+					toggleChat();
+				}
+			}
+		})
+		.resize();
 });
 
-
-/* Youtube embed resize */
-$(window).resize(function() {
-    if (!($(".iframe-youtube").hasClass("responsive-iframe"))) {
-        var width = $(".iframe-youtube").width();
-        var height = width * 0.5625; // 16:9 ratio
-        $(".iframe-youtube").css("height", height + "px");
-    }
-}).resize();
+$(window)
+	.resize(function () {
+		if ($(window).width() >= 1100) {
+			if (!$(".iframe-youtube").hasClass("responsive-iframe")) {
+				var width = $(".iframe-youtube").width();
+				var height = width * 0.5625; // 16:9 ratio
+				$(".iframe-youtube").css("height", height + "px");
+			}
+		} else {
+			$(".iframe-youtube").css("width", "655px");
+			$(".iframe-youtube").css("height", "370px");
+		}
+	})
+	.resize();
